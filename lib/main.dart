9d3d2278c9414fd55a9d5e5b5f4f7d0b321bc927
Justin Hoang <72476123+suasuasuasuasua@users.noreply.github.com>
2023-06-29 16:19:31 -0600

    Removed unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 /// https://console.firebase.google.com/u/0/
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
 import 'package:notes_app/views/login_view.dart';
-import 'package:notes_app/views/verify_email_view.dart';
 import 'package:notes_app/firebase/firebase_options.dart';
-import 'package:notes_app/views/register_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
 import 'package:notes_app/util/widget_builder.dart';
 
 class LoginView extends StatefulWidget {
