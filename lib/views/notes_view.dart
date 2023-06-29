c4d71f2b92874afae1c3b4ab655a63a14f987913
Justin Hoang <72476123+suasuasuasuasua@users.noreply.github.com>
2023-06-29 17:31:06 -0600

    Created notes view with logout button on appbar

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,39 @@
+import 'dart:developer' show log;
+
+import 'package:flutter/material.dart';
+
+enum MenuAction { logout }
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notes'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (value) {
+              log(value.toString());
+            },
+            itemBuilder: (context) {
+              return [
+                const PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout,
+                  child: Text('Logout'),
+                )
+              ];
+            },
+          )
+        ],
+      ),
+      body: const Text('Hello world!'),
+    );
+  }
+}
